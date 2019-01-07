3d643be059970081dbb1c753b1af7497a4e37341
DPB <5628465+deepybee@users.noreply.github.com>
2019-01-07 17:40:01 +0100

    Updated variable declarations for Spectre x360 13-ae015nd

--- rotate.py
+++ rotate.py
@@ -15,9 +15,9 @@ from glob import glob
 
 
 # Your device names go here if different.
-TOUCHSCREEN = 'ELAN0732:00 04F3:2493'
+TOUCHSCREEN = 'ELAN2514:00 04F3:2591'
 TOUCHPAD    = 'SynPS/2 Synaptics TouchPad'
-PEN         = 'ELAN0732:00 04F3:22E1 Pen Pen (0)'
+PEN         = 'ELAN2514:00 04F3:2591 Pen Pen (0)'
 
 disable_touchpads = True
 
